7c44b435a69274b2a1a81bb92ae75524a7863605
Grigorii Horos <horosgrisa@gmail.com>
2023-11-11 16:17:59 +0200

    Update plugin

--- iterm.plugin.zsh
+++ iterm.plugin.zsh
@@ -4,7 +4,7 @@ ZSH_CACHE_DIR="${ZSH_CACHE_DIR:-${TMPDIR:-/tmp}/zsh-${UID:-user}}"
 ITERM_CACHE_FILE="${ZSH_CACHE_DIR}/iterm.zsh"
 
 source "${ITERM_CACHE_FILE}" 2>/dev/null || {
-  test -e "${HOME}/.iterm2_shell_integration.zsh" && cat "${HOME}/.iterm2_shell_integration.zsh" >> "${ITERM_CACHE_FILE}"
+  test -e "${HOME}/.iterm2_shell_integration.zsh" && cat "${HOME}/.iterm2_shell_integration.zsh" > "${ITERM_CACHE_FILE}"
 
   source "${ITERM_CACHE_FILE}"
 }
